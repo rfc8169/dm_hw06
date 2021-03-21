87dacb2d104e5cec5f7bda41e276b675a0b1e812
nmr3095 <nmr3095@rit.edu>
2021-03-21 12:28:24 -0400

    Started to implement the cross correlation calculation method in the Part A file.

--- HW06/HW06_Part_A_Cross_Correlation.py
+++ HW06/HW06_Part_A_Cross_Correlation.py
@@ -3,3 +3,19 @@ Assignment: HW06
 Description:
 Author: Reed Cogliano, Neel Raj
 """
+
+import pandas
+
+def calculate_cross_correlation(csv_file):
+    """
+
+    """
+    student_df = pandas.read_csv(csv_file)
+    print(student_df)
+
+
+if __name__ == '__main__':
+    """
+    
+    """
+    calculate_cross_correlation("test.csv")
