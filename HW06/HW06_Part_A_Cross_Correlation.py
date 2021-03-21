12b58c3b6aa0656cf12cd0c1e483b761934ad35a
nmr3095 <nmr3095@rit.edu>
2021-03-21 15:03:07 -0400

    Finished implementation of cross-correlation part of the homework. Created a file of the correlation table.

--- HW06/HW06_Part_A_Cross_Correlation.py
+++ HW06/HW06_Part_A_Cross_Correlation.py
@@ -10,8 +10,13 @@ def calculate_cross_correlation(csv_file):
     """
 
     """
-    student_df = pandas.read_csv(csv_file)
-    print(student_df)
+    sss_df_full = pandas.read_csv(csv_file)
+    sss_df = sss_df_full.drop(['ID'], axis=1)
+    correlation_table = sss_df.corr(method='pearson')\
+
+    correlation_table.to_csv('correlation_table.csv', index=False)
+
+    print(correlation_table)
 
 
 if __name__ == '__main__':
