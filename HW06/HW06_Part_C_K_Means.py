dd3e5feeafa15b224108072e9309285acf282735
Reed Cogliano <rfc8169@rit.edu>
2021-03-22 11:49:49 -0400

    Made a kmeans implementation with sklearn

--- HW06/HW06_Part_C_K_Means.py
+++ HW06/HW06_Part_C_K_Means.py
@@ -3,3 +3,29 @@ Assignment: HW06
 Description:
 Author: Reed Cogliano, Neel Raj
 """
+from sklearn.cluster import KMeans
+import numpy as np
+import pandas
+
+
+def prepare_kmeans(csv_file):
+    clusters = []
+    student_df = pandas.read_csv(csv_file)
+    for cluster_index in range(len(student_df)):
+        array_from_df = student_df.iloc[cluster_index].to_numpy()
+        clusters.append(np.array(array_from_df))
+    kmeans_clustering(clusters)
+
+
+def kmeans_clustering(clusters):
+    number_of_clusters = 6
+    kmeans = KMeans(number_of_clusters)
+    kmeans.fit(clusters)
+    print(kmeans.cluster_centers_)
+
+
+if __name__ == '__main__':
+    """
+    Expects the first argument when calling the program to be the filename of the csv data
+    """
+    prepare_kmeans("test.csv")
